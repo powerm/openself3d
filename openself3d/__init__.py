6800cb4e62c409ed5594c8057a75bbd6a8df8464
johnny <powermwhl@gmail.com>
2022-03-04 10:54:47 +0800

    add: add the init and version

---
+++ openself3d/__init__.py
@@ -0,0 +1,7 @@
+
+from  .datasets import *
+from  .hooks import *
+from  .models import *
+from  .ops import * 
+from .utils import * 
+from .version import * 
---
+++ openself3d/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Sat May 15 00:43:53 2022
+
+__version__ = '0.0.1+332'
+short_version = '0.0.1'
